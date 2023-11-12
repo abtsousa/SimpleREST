0c8779af73d74126a519e81bf72793323c8020b0
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-11-12 12:24:29 +0000

    working prototype

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Setup
 
-1) Setup your Python environment (e. g. with Pyenv on Linux):
+1) Setup your Python environment (e. g. with Pyenv on Linux) and install related dependencies:
 
 ```bash
 pyenv local 3.12
@@ -24,5 +24,5 @@ uvicorn RandomAppAPI.main:app --reload
 3) Test the API by sending a GET request to the IP address of the live server (e.g. cURL):
 
 ```bash
-curl http://127.0.0.1:8000/
+curl http://127.0.0.1:8000/suggest_app
 ```
--- RandomAppAPI/main.py
+++ RandomAppAPI/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+import random
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def print_test():
-    return {"message": "Hello, world!"}
+def get_random_app_name() -> str:
+    """Auxiliary method that returns a random app name from a hard-coded list."""
+    apps = ["Evony: The King's Return", "Lords Mobile: Kingdom Wars", "Clash of Clans"] # The hard-coded list.
+    return random.choice(apps)
+
+
+
+@app.get("/suggest_app")
+async def suggest_app():
+    return {"app_name": get_random_app_name()}
