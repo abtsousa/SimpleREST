abd54519ed0ba8e666934eb78f334964e6a566df
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-11-12 20:07:23 +0000

    format code

--- RandomAppAPI/main.py
+++ RandomAppAPI/main.py
@@ -1,7 +1,7 @@
 # Imports
-from fastapi import FastAPI # FastAPI
+from fastapi import FastAPI  # FastAPI
 
-from RandomAppAPI.routers.suggest_app import router as suggest_app # Our API
+from RandomAppAPI.routers.suggest_app import router as suggest_app  # Our API
 
 # App metadata
 __author__ = "Afonso Bras Sousa"
@@ -27,7 +27,7 @@ app = FastAPI(
     version="0.1.0",
     contact={
         "name": "Afonso Bras Sousa",
-    }
+    },
 )
 
 # Include our suggest_app APIRouter
